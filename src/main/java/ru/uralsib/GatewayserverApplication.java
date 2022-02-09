ddf3c6e676649f774ff1519fee224cb079f3e503
pitver <pitver2014@yandex.ru>
2022-02-09 10:11:03 +0300

    first commit

---
+++ src/main/java/ru/uralsib/GatewayserverApplication.java
@@ -0,0 +1,15 @@
+package ru.uralsib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+}
