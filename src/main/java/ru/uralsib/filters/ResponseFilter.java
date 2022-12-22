4b24f7ac88bfe1da123330b327148c06959bfdfb
Petr Vershinin <p.vershinin@yandex.ru>
2022-12-22 08:17:51 +0300

    add filters

---
+++ src/main/java/ru/uralsib/filters/ResponseFilter.java
@@ -0,0 +1,30 @@
+package ru.uralsib.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class ResponseFilter {
+ 
+    final Logger logger =LoggerFactory.getLogger(ResponseFilter.class);
+    
+    @Autowired
+	FilterUtils filterUtils;
+ 
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> chain.filter(exchange).then(Mono.fromRunnable(() -> {
+              HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+              String correlationId = filterUtils.getCorrelationId(requestHeaders);
+              logger.info("Adding the correlation id to the outbound headers. {}", correlationId);
+              exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+              logger.info("Completing outgoing request for {}.", exchange.getRequest().getURI());
+          }));
+    }
+}
---
+++ src/main/java/ru/uralsib/filters/TrackingFilter.java
@@ -0,0 +1,57 @@
+package ru.uralsib.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Order(1)
+@Component
+public class TrackingFilter implements GlobalFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
+
+	@Autowired
+	FilterUtils filterUtils;
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+		HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+		if (isCorrelationIdPresent(requestHeaders)) {
+			logger.info("tmx-correlation-id found in tracking filter: {}. {},{},{}",
+					filterUtils.getCorrelationId(requestHeaders),
+					exchange.getRequest().getLocalAddress(),exchange.getRequest().getRemoteAddress(),
+					exchange.getRequest().getQueryParams());
+		} else {
+			String correlationID = generateCorrelationId();
+			exchange = filterUtils.setCorrelationId(exchange, correlationID);
+			//String ipAddress = requestHeaders.("X-FORWARDED-FOR")
+			logger.info("tmx-correlation-id generated in tracking filter: {}.{},{},{}", correlationID,
+					exchange.getRequest().getLocalAddress(),exchange.getRequest().getRemoteAddress(),
+					exchange.getRequest().getQueryParams());
+		}
+		
+		return chain.filter(exchange);
+	}
+
+
+	private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+		if (filterUtils.getCorrelationId(requestHeaders) != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private String generateCorrelationId() {
+		return java.util.UUID.randomUUID().toString();
+	}
+
+}
